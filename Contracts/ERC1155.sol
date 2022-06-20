40314652a7209f93a540a8a7cb6bb1b5bc246245
Ahrenfullstop <aj_posthumus@yahoo.com>
2022-06-20 22:23:09 +0200

    Added structure and ERC1155 contract

---
+++ Contracts/ERC1155.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+/**
+ * @dev {ERC1155} token, including:
+ *
+ *  - ability for holders to burn (destroy) their tokens
+ *  - a minter role that allows for token minting (creation)
+ *  - a pauser role that allows to stop all token transfers
+ *
+ * This contract uses AccessControl to lock permissioned functions using the
+ * different roles.
+ *
+ * The account that deploys the contract will be granted the minter and pauser
+ * roles, as well as the default admin role, which will let it grant both minter
+ * and pauser roles to other accounts.
+ */
+
+
+/// @custom:security-contact security@momint.so
+contract MomintToken is Initializable, ERC1155Upgradeable, AccessControlUpgradeable, PausableUpgradeable, ERC1155BurnableUpgradeable, ERC1155SupplyUpgradeable {
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC1155_init("ipfs.io/"); //Our initial URI structure
+        __AccessControl_init();
+        __Pausable_init();
+        __ERC1155Burnable_init();
+        __ERC1155Supply_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender); // Grant default admin role to deployer
+        _grantRole(URI_SETTER_ROLE, msg.sender); //to control setting UR's
+        _grantRole(PAUSER_ROLE, msg.sender); // to pause transfers
+        _grantRole(MINTER_ROLE, msg.sender); // to mint tokens
+    }
+
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    /**
+     * @dev Creates `amount` new tokens for `to`, of token type `id`. 
+     * @param account: the public key to deliver this token to
+     * @param id: @todo not sure yet if we have to generate this with enumerable?
+     * @param amount: the amount of tokens to mint
+     * @param data: _mint just passes data without any modification, a particular deployment could overwrite this method and do something with that data, otherwise its optional.
+     * 
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mint(account, id, amount, data);
+    }
+
+    /**
+    * @dev Batch version of minting, Creates `amount` new tokens for `to`, of token types `ids`.
+    */
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(
+        address operator, 
+        address from, 
+        address to, 
+        uint256[] memory ids, 
+        uint256[] memory amounts, 
+        bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155Upgradeable, ERC1155SupplyUpgradeable)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155Upgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
